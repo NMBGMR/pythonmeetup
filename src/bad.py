1bdbf9391746f36e4adee7830609507a6e42f0ac
Jake Ross <jirhiker@gmail.com>
2017-11-01 20:47:36 -0600

    added good/bad examples for printing contents of a file

---
+++ src/bad.py
@@ -0,0 +1,32 @@
+file = open('/a/b/c.txt', 'r')
+lines = file.readlines()
+
+c = 0
+for i in range(len(lines)):
+    print c, lines[c]
+    c = c+1
+
+
+
+# Annotated
+# no "if __name__..."
+# code not in a function
+# no license
+# file is a reversed word in python
+# use with open(... instead of f = open(...
+# avoid hardcoded paths. use os.path.join and string formatting
+file = open('/a/b/c.txt', 'r')
+
+# files are iterable in python
+lines = file.readlines()
+
+# in python 2.7 use xrange instead of range
+# use enumerate() instead of a counter
+c = 0
+for i in range(len(lines)):
+    # use string formatting and use prefix
+    print c, lines[c]
+    c = c+1
+
+# file not closed
+# no EOF
---
+++ src/good.py
@@ -0,0 +1,28 @@
+# ===============================================================================
+# Copyright 2017 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+def print_file(path):
+    print 'Printing contents of {}'.format(path)
+    with open(path, 'r') as rfile:
+        for i,line in enumerate(rfile):
+            print 'Line#={}, Value={}'.format(i, line)
+
+
+if __name__ == '__main__':
+    test_path = 'test_file.txt'
+    print_file(test_path)
+
+# ============= EOF =============================================
